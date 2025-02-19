faea63659a6f3b4145a2467cd6dc2448df88077f
Jangseyoung0 <young240956@gmail.com>
2025-02-19 22:38:03 +0900

    Set: mypage branch 생성

--- src/main/java/kr/co/moments/goods/GoodsMapper.java
+++ src/main/java/kr/co/moments/goods/GoodsMapper.java
@@ -1,5 +1,8 @@
 package kr.co.moments.goods;
+import org.apache.ibatis.annotations.Mapper;
 
+
+@Mapper
 public interface GoodsMapper {
 
 }
--- src/main/java/kr/co/moments/users/UsersMapper.java
+++ src/main/java/kr/co/moments/users/UsersMapper.java
@@ -1,5 +1,8 @@
 package kr.co.moments.users;
+import org.apache.ibatis.annotations.Mapper;
 
+
+@Mapper
 public interface UsersMapper {
 
 }
