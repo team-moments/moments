4ca368bd2ec30ba8af72be702c98419e7faa6218
yughh <dbrud1104@daum.net>
2025-03-11 18:55:40 +0900

    Feat: 쿠팡 파트너스 API로 상품 가져오

--- src/main/java/kr/co/moments/domain/GoodsVO.java
+++ src/main/java/kr/co/moments/domain/GoodsVO.java
@@ -5,8 +5,9 @@ import lombok.Data;
 @Data
 public class GoodsVO {
     private int goods_no;
+    private int goods_id;
     private String goods_name;
     private String goods_image;
-    private int category_middle_no;
+    private int category_no;
     private int wish_cnt;
 }
